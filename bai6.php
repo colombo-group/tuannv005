ef4b2818e4dca9c9ba4ba216c2a2dcf0c22ae81f
Tuan93bn <tuantbbn@gmail.com>
2016-06-03 11:35:28 +0700

    Update bai6.php

--- bai6.php
+++ bai6.php
@@ -24,7 +24,7 @@
                 }
                 
                 if(isset($_POST['divi'])) {
-                    if($value2 == 0) {
+                    if($_POST['value2'] == 0) {
                         $comment="Khong ton tai phep chia cho 0. Vui long nhap lai Value2!=0";
                     }else {
                         $result = $value1 / $value2;
