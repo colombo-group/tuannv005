2765a63f95bf6657cd69edf5be366eb82a091c12
Tuan93bn <tuantbbn@gmail.com>
2016-06-03 11:42:58 +0700

    Update bai6.php

--- bai6.php
+++ bai6.php
@@ -24,7 +24,7 @@
                 }
                 
                 if(isset($_POST['divi'])) {
-                    if($_POST[$value2] == 0) {
+                    if($value2 == 0) {
                         $comment="Khong ton tai phep chia cho 0. Vui long nhap lai Value2!=0";
                         $result = "err";
                     }else {
